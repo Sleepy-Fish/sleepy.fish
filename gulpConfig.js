3bc909e43b145a1005dbad7c3e7b7b06fce9fbba
James Harrington <JRileyHarrington@gmail.com>
2018-01-28 14:54:32 -0500

    layout build system

---
+++ gulpConfig.js
@@ -0,0 +1,25 @@
+module.exports = {
+    port: 9005,
+    protocol: 'http',
+    domain: 'localhost',
+    paths: {
+        html: './src/*.html',
+        js: './src/js/**/*.js',
+        css: './src/css/**/*.css',
+        vendor: {
+            js: [
+                './node_modules/jquery/dist/jquery.min.js',
+                './node_modules/jquery.easing/jquery.easing.min.js',
+                './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+            ],
+            css: [
+                'node_modules/bootstrap/dist/css/bootstrap.min.css',
+                'node_modules/bootstrap/dist/css/bootstrap-theme.min.css',
+                'node_modules/toastr/build/toastr.min.css'
+            ]
+        },
+        img: './src/img/*',
+        dist: './dist',
+        entry: './src/index.js'
+    }
+}
--- gulpFile.js
+++ gulpFile.js
@@ -1,24 +1,81 @@
 var gulp = require('gulp');
+var connect = require('gulp-connect');
+var open = require('gulp-open');
 var concat = require('gulp-concat');
+var lint = require('gulp-eslint');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+
+var config = require('./gulpConfig');
+var baseurl = config.protocol+'://'+config.domain;
+
+gulp.task('connect', function(){
+    connect.server({
+        root: ['dist'],
+        host: config.domain,
+        port: config.port,
+        base: baseurl,
+        livereload: true
+    });
+});
+
+gulp.task('open', ['connect'], function(){
+    gulp.src('dist/index.html')
+        .pipe(open({uri: baseurl + ':' + config.port + '/'}));
+});
+
+gulp.task('lint', function() {
+    return gulp.src(config.paths.js)
+        .pipe(lint({configFile: 'eslint.config.json'}))
+        .pipe(lint.format());
+})
+
+gulp.task('html', function(){
+    gulp.src(config.paths.html)
+        .pipe(gulp.dest(config.paths.dist))
+        .pipe(connect.reload());
+});
+
+gulp.task('js', function(){
+    browserify(config.paths.entry)
+        .transform(babelify)
+        .bundle()
+        .on('error', console.error.bind(console))
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest(config.paths.dist + '/js'))
+        .pipe(connect.reload());
+});
+
+gulp.task('css', function(){
+    gulp.src(config.paths.css)
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest(config.paths.dist + '/css'))
+});
+
+gulp.task('img', function(){
+    gulp.src(config.paths.img)
+        .pipe(gulp.dest(config.paths.dist + '/img'));
+})
 
 gulp.task('vendor-js', function() {
-    gulp.src([
-        './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
-        './node_modules/jquery/dist/jquery.min.js',
-        './node_modules/jquery.easing/jquery.easing.min.js'
-        ])
+    gulp.src(config.paths.vendor.js)
         .pipe(concat('vendor.js'))
-        .pipe(gulp.dest('./dist/vendor'));
+        .pipe(gulp.dest('./dist/js'));
 });
 
 gulp.task('vendor-css', function() {
-    gulp.src([
-        './node_modules/bootstrap/dist/css/bootstrap.min.css'
-        ])
+    gulp.src(config.paths.vendor.css)
         .pipe(concat('vendor.css'))
-        .pipe(gulp.dest('./dist/vendor'));
+        .pipe(gulp.dest('./dist/css'));
 });
 gulp.task('vendor', ['vendor-js', 'vendor-css']);
 
-// Default task
-gulp.task('default', ['vendor']);
+
+gulp.task('watch', function(){
+    gulp.watch(config.paths.html, ['html']);
+    gulp.watch([config.paths.entry, config.paths.js], ['js', 'lint']);
+    gulp.watch(config.paths.css, ['css'])
+});
+
+gulp.task('default', ['open', 'html', 'js', 'css', 'vendor', 'img', 'lint', 'watch']);
