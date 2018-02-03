e8204b1dc16c95958e4dec24a3a9afc4146cfc88
J Riley Harrington <jrileyharrington@gmail.com>
2018-02-03 10:39:32 -0500

    materialize setup

--- gulpConfig.js
+++ gulpConfig.js
@@ -8,16 +8,17 @@ module.exports = {
         css: './src/scss/**/*.scss',
         vendor: {
             js: [
-                './node_modules/jquery/dist/jquery.min.js'
+                './node_modules/jquery/dist/jquery.min.js',
+                './node_modules/materialize-css/dist/js/materialize.js'
             ],
             css: [
-                './node_modules/font-awesome/css/font-awesome.min.css'
+                './node_modules/font-awesome/css/font-awesome.min.css',
+                './node_modules/materialize-css/dist/css/materialize.min.css'
             ],
-            scss: [
-                
-            ],
+            scss: [],
             fonts: [
-                './node_modules/font-awesome/fonts/fontawesome-webfont.*'
+                './node_modules/font-awesome/fonts/fontawesome-webfont.*',
+                './node_modules/materialize-css/dist/fonts/roboto/*'
             ]
         },
         img: './src/img/*',
