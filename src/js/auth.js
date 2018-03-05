1262aa1785eabb31317332b6c380fe0a4c1b6a8c
James Harrington <JRileyHarrington@gmail.com>
2018-03-04 21:14:06 -0500

    added auth lock from auth0

--- src/js/auth.js
+++ src/js/auth.js
@@ -8,16 +8,14 @@ var lock = new Auth0Lock(
         socialButtonStyle: "small",
         languageDictionary: {"title":"Sleepy Fish"},
         language: "en",
-        theme: {"logo":"http://sleepy.fish/img/angel-fish.svg","primaryColor":"#64B5F6"}
+        theme: {"logo":"https://sleepy.fish/img/angel-fish.svg","primaryColor":"#64B5F6"}
     }
 );
 
 lock.on("authenticated", function(authResult) {
-    // Use the token in authResult to getUserInfo() and save it to localStorage
     lock.getUserInfo(authResult.accessToken, function(error, profile) {
       if (error) {
         throw Error(error);
-        return;
       }
       console.log(authResult.accessToken)
       console.log(profile);
