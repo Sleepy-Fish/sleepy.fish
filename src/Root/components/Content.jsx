e254d5f213c9496e86681e0593dc702f03f59b9e
Riggy <riley@sleepy.fish>
2021-10-01 13:11:28 -0400

    kick off build

--- src/Root/components/Content.jsx
+++ src/Root/components/Content.jsx
@@ -4,7 +4,7 @@ const Content = () => (
   <div className="absolute top-0 left-0 w-screen h-screen p-5 md:p-10 pt-header-m md:pt-header-d pb-footer-m md:pb-footer-d">
     <div className="flex items-center justify-center h-full">
       <p className="text-center text-highlight">
-        This site is under construction. There may be a demo running that you can find
+        This site is under construction. There may be a demo that you can find
         {' '}
         <a className="underline text-primary" href="https://demo.sleepy.fish">here</a>
         .
