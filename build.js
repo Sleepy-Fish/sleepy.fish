d81f82e28cab45282ccdb23cbd1d939e804358a6
Caleb Rogers <caleb@calebjay.com>
2021-11-18 16:56:01 +0800

    Working with tailwind
    Ok so, for now, the only way I could get this to work is to have tailwind run
    seperately, and have it handle outputting our css file. Luckily, it seems it can
    handle scss files, so maybe we can just do that? Even luckier, it seems to be
    automatically pruning and tree-shaking, so the output CSS is not monumental. How
    it knows, I have no idea.

--- build.js
+++ build.js
@@ -1,12 +1,16 @@
-const vuePlugin = require('esbuild-vue');
 const  sassPlugin =  require("esbuild-sass-plugin").sassPlugin;
+const autoprefixer = require("autoprefixer");
+const postCssPlugin = require("@deanc/esbuild-plugin-postcss");
 
 require('esbuild').build({
   entryPoints: ['src/index.jsx'],
   bundle: true,
   outfile: 'dist/out.js',
   loader: { '.ttf': 'file'},
-  plugins: [vuePlugin(), sassPlugin()],
+  // plugins: [sassPlugin(),
+  //              postCssPlugin({
+  //                plugins: [autoprefixer, require('tailwindcss')],
+  //              }), ],
   define: {
     "process.env.NODE_ENV": JSON.stringify("development"),
   },
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './Root';
-import './index.scss';
+// import './index.scss';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
