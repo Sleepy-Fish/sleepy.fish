d8b477a142b586f4ccdae4606a84d5bd26246051
Caleb Rogers <caleb@calebjay.com>
2021-11-19 16:15:54 +0800

    Moar responsive footer

--- src/Root/components/Footer.jsx
+++ src/Root/components/Footer.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 const Footer = () => (
   <nav className="absolute bottom-0 left-0 flex flex-row items-center w-full bg-black h-footer ">
-    <div className="absolute top-0 w-full h-1 bg-primary bg-gradient-to-r from-primary via-highlight to-secondary" />
-    <div className="flex items-center justify-center w-1/3 h-full ml-2 text-lg font-light text-center text-primary hover:text-primary">
-      riley@sleepy.fish
-    </div>
-    <div className="flex items-center justify-center w-1/3 h-full ml-2 text-lg font-light text-center text-primary hover:text-primary">
-      caleb@calebjay.com
-    </div>
+    <div className="absolute top-0 w-full h-2 bg-primary bg-gradient-to-r from-primary via-highlight to-secondary" />
+    <div className="w-full flex flex-row flex-wrap justify-evenly">
+      <span className="ml-2 text-lg font-light text-center text-primary hover:text-primary">
+        riley@sleepy.fish
+      </span>
+      <span className="ml-2 text-lg font-light text-center text-primary hover:text-primary">
+        caleb@calebjay.com
+      </span>
+  </div>
   </nav>
 );
 
